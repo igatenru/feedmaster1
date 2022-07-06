08240acbb6863a839e1e1e487b3aaf46528ca031
Andrey Gleba <52205645+igatenru@users.noreply.github.com>
2022-07-06 22:49:09 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ include_once "inc/file_upload.inc.php";
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Загрузка фида</title>
+	<title>Конвертор товарных фидов YML в формат Google и Facebook</title>
 </head>
 
 <body>
-<h1>Загрузка фида на сервер</h1>
-    
+<h1>Конвертор товарных фидов YML (Yandex) в формат Google</h1>
+<p>Конвертирует <a href="https://yandex.ru/support/marketplace/assortment/auto/yml.html" target="_blanc">товарный фид Яндекса (YML)</a>
+в фид формата Google (такой же использует Facebook и Instagram). Автоматически добавляет недостающие секции, корректно устанавливает oldprice (со сроком акции)
+делает улучшение названия товара, где это возможно.</p>   
 <br>
 <br>
 <form enctype="multipart/form-data" method="post" action="converter.php">  
@@ -19,7 +21,7 @@ include_once "inc/file_upload.inc.php";
     <input type="file" name="user_feed" />
     </br>
     </br>
-    <input type="submit" name="uploadBtn" value="Загрузить YML файл" />
+    <input type="submit" name="uploadBtn" value="Конвертировать в Google XML" />
 </form>
 
 
