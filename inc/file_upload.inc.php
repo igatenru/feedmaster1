428e9ac9596b1fad09d45b4ad279801446647c23
Andrey Gleba <52205645+igatenru@users.noreply.github.com>
2022-07-08 13:12:57 +0300

    Uploading feed via URL added

--- inc/file_upload.inc.php
+++ inc/file_upload.inc.php
@@ -2,64 +2,73 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_REQUEST['uploadBtn'])))
 {
-    $type = $_FILES['user_feed']['type'];
-    $size = $_FILES['user_feed']['size'];
-    $tmp = $_FILES['user_feed']['tmp_name'];
-    $file_name = basename($_FILES['user_feed']['name']);
     $upload_dir = "feeds/";
-    $whitelist = array("xml", "yml");
-    $file_extension = pathinfo($file_name, PATHINFO_EXTENSION);
 
-    if (! in_array($file_extension, $whitelist))
+    
+    if (! empty($_POST['feed_url']))
     {
-        echo "Загружен файл неверного формата $file_extension</br></br>" . "Загрузите .xml файл";
-        exit;
+        
+        $feed_url = htmlspecialchars($_POST['feed_url']);
+        $file_name = basename($feed_url);
+
+        if (file_put_contents("feeds/$file_name", file_get_contents($feed_url)))
+        {
+            echo "Фид успешно загружен";
+        }
     }
     else
-    {
-        /* if ($type != "text/xml" || $type != "application/xml")
+    { 
+        $type = $_FILES['user_feed']['type'];
+        $size = $_FILES['user_feed']['size'];
+        $tmp = $_FILES['user_feed']['tmp_name'];
+        $file_name = basename($_FILES['user_feed']['name']);
+        $whitelist = array("xml", "yml");
+        $file_extension = pathinfo($file_name, PATHINFO_EXTENSION);
+
+        if (! in_array($file_extension, $whitelist))
         {
-            echo "Загружен файл неверного формата" . PHP_EOL . "Загрузите .xml файл";
+            echo "Загружен файл неверного формата $file_extension</br></br>" . "Загрузите .xml файл";
+            exit;
         }
         else
-        { */
-            if ($size > 100000000)
+        {
+            /* if ($type != "text/xml" || $type != "application/xml")
             {
-                echo "Загружен файл слишком большого размера. Допустимый размер: до 100 Мб.";
-                exit;
+                echo "Загружен файл неверного формата" . PHP_EOL . "Загрузите .xml файл";
             }
             else
-            {
-                if (!is_writable($upload_dir))
+            { */
+                if ($size > 100000000)
                 {
-                    echo "Папка " . $upload_dir . " не имеет прав на запись";
+                    echo "Загружен файл слишком большого размера. Допустимый размер: до 100 Мб.";
                     exit;
                 }
                 else
                 {
-                    $copy_file = copy($tmp, ($upload_dir . $file_name));
-                    if (! $copy_file)
+                    if (!is_writable($upload_dir))
                     {
-                       echo "Возникла ошибка, файл не удалось загрузить!";
-                       exit;
+                        echo "Папка " . $upload_dir . " не имеет прав на запись";
+                        exit;
                     }
                     else
                     {
-                        chmod(($upload_dir . $file_name), 0644);
-                        echo "Файл успешно загружен!";
+                        $copy_file = copy($tmp, ($upload_dir . $file_name));
+
+                        if (! $copy_file)
+                        {
+                        echo "Возникла ошибка, файл не удалось загрузить!";
+                        exit;
+                        }
+                        else
+                        {
+                            chmod(($upload_dir . $file_name), 0644);
+                            echo "Файл успешно загружен!";
+                        }
                     }
                 }
-            }
-        //}
-    }
+            //}
+        }
+    }  
 
-
-
-    /*
-    chmod("$tmp/$file_name", 0644);
-    move_uploaded_file($tmp, "feeds/$file_name");
-    chmod("feeds/$file_name", 0644);
-    */
-}  
-    
+}    
 ?>
